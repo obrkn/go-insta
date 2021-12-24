ff3bb4732f7873e8d8e794c088a11fc04085ad4e
obr-note <k33mdukb9n@yahoo.co.jp>
2021-12-24 21:59:54 +0900

    #4 signup

---
+++ front/pages/signup.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+// import Image from 'next/image';
+import { Container, Box, Button } from '@mui/material';
+import axios from 'axios';
+import { useState, useCallback } from 'react';
+
+const SignUp: NextPage = () => {
+  const [data, setData] = useState("まだ何もデータ来てないよう")
+  const getData = useCallback(
+    () => {
+      axios
+        .get("http://localhost:8080")
+        .then(res => setData(res.data))
+        // .get("back:8080", { 
+        //   headers: {'withCredentials': 'true'}
+        // })
+        // .then(res => setData(res.data))
+    },
+    [],
+  )
+  return (
+    <Container maxWidth="sm">
+      <Head>
+        <title>tiwitter</title>
+        <meta name="description" content="Goを使ったインスタ風アプリです。" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Box sx={{ m: 2 }}>
+          {data}
+        </Box>
+        <Button onClick={() => getData()}>押して！</Button>
+      </main>
+
+      <footer>
+      　あああああ
+      </footer>
+    </Container>
+  )
+}
+
+export default SignUp
