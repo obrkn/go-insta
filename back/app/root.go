51a042836b2f7c554655d48869d0b9650d42ef1c
obr-note <k33mdukb9n@yahoo.co.jp>
2022-01-30 16:17:06 +0900

    dev-015 introduce gorilla/csrf and fix cors error

--- back/app/root.go
+++ back/app/root.go
@@ -27,13 +27,24 @@ func Root() {
 
 	r := mux.NewRouter()
 
-	csrfMiddleware := csrf.Protect([]byte(verifyKey), csrf.Secure(false))
+	csrfMiddleware := csrf.Protect(
+		[]byte(verifyKey),
+		csrf.RequestHeader("X-CSRF-Token"),
+		csrf.Secure(false),
+		csrf.TrustedOrigins([]string{"*localhost*"}),
+	)
 	api := r.PathPrefix("/api").Subrouter()
 	api.Use(csrfMiddleware)
 
+	api.HandleFunc("/token", token).Methods(http.MethodGet)
 	api.HandleFunc("/home", HomeHandler).Methods(http.MethodGet)
+	api.HandleFunc("/home", HomeHandler).Methods(http.MethodPost)
+	api.HandleFunc("/post", post).Methods(http.MethodPost)
+	api.HandleFunc("/post", post).Methods(http.MethodOptions)
 	api.HandleFunc("/signup", SignUpHandler).Methods(http.MethodPost)
 	api.HandleFunc("/login", LoginHandler).Methods(http.MethodPost)
+	// r.HandleFunc("/api/token", token).Methods(http.MethodGet)
+	// r.HandleFunc("/api/post", post)
 	r.HandleFunc("/favicon.ico", func(rw http.ResponseWriter, r *http.Request) {})
 
 	http.ListenAndServe(":8080", r)
@@ -42,5 +53,32 @@ func Root() {
 	// 	log.Fatal(http.ListenAndServe(":8080", csrf.Protect(verifyKey, csrf.Secure(false))(r)))
 	// } else {
 	// 	log.Fatal(http.ListenAndServe(":8080", csrf.Protect(verifyKey)(r)))
+	// }
+}
+
+func token(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
+	w.Header().Add("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Add("Access-Control-Expose-Headers", "X-CSRF-Token")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("X-CSRF-Token", csrf.Token(r))
+	w.Write([]byte("hello, rosk"))
+}
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
+	w.Header().Add("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	// if r.Method == "POST" {
+	// 	db := DbConnect()
+	// 	defer db.Close()
+
+	// 	_, err := db.Exec("INSERT INTO test(content) VALUES(?);", "OK, boomer")
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Fprint(w, "ok your command")
 	// }
 }
