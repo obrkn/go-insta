c66cd30dbc5e1a0e2a9b6ab401221f3451c3849c
obr-note <k33mdukb9n@yahoo.co.jp>
2022-02-13 18:12:58 +0900

    dev-018 change go style

--- back/go.mod
+++ back/go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/csrf v1.7.1
 	github.com/gorilla/mux v1.8.0
-	github.com/gorilla/sessions v1.2.1
 	github.com/joho/godotenv v1.4.0
 	github.com/pkg/errors v0.9.1
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
--- back/utils/logic/response.logic.go
+++ back/utils/logic/response.logic.go
@@ -21,6 +21,11 @@ func NewResponseLogic() ResponseLogic {
 	APIレスポンス送信処理
 */
 func (rl *responseLogic) SendResponse(w http.ResponseWriter, response []byte, code int) {
+	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
+	w.Header().Add("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Add("Access-Control-Expose-Headers", "X-CSRF-Token")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
 	w.WriteHeader(code)
 	w.Write(response)
 }
